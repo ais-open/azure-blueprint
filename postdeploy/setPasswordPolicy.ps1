0d2cd3de35e5cd99228588f974491279d6df00c3
Harun Davood <davoodharun@gmail.com>
2017-05-09 21:57:00 -0400

    added password policy restriction

--- postdeploy/setPasswordPolicy.ps1
+++ postdeploy/setPasswordPolicy.ps1
@@ -3,33 +3,32 @@ $Domain = (gwmi WIN32_ComputerSystem).Domain
 
 Import-Module ActiveDirectory
 
-#Provide Domain admin credentials
+<#
+#Domain admin credentials
 $User='aisadmin'
 $Password='Azuresample123$'
-$UserDomain=$Domain+í\í+$User
-$SecurePassword=Convertto-SecureString ñString $Password ñAsPlainText ñforce
-
+$UserDomain=$Domain+’\’+$User
+$SecurePassword=Convertto-SecureString –String $Password –AsPlainText –force
 #PS credentials
 $AdminCredentials=New-object System.Management.Automation.PSCredential $UserDomain,$SecurePassword
+#>
+
 
 #this etting errors out
 #Setting password policy
 #Set-ADDefaultDomainPasswordPolicy -Identity $Domain -AuthType Basic -Credential $AdminCredentials -MaxPasswordAge 60.00:00:00 -MinPasswordAge 1.00:00:00 -PasswordHistoryCount 24 -ComplexityEnabled $true -ReversibleEncryptionEnabled $true -MinPasswordLength 14
 
-
-#Setting password policy for domain users- with auth type as "Negotiate"
 #this does not error out
-Set-ADDefaultDomainPasswordPolicy -Identity $domain -AuthType Negotiate -MaxPasswordAge 60.00:00:00 -MinPasswordAge 1.00:00:00 -PasswordHistoryCount 24 -ComplexityEnabled $true -ReversibleEncryptionEnabled $true -MinPasswordLength 14
+Set-ADDefaultDomainPasswordPolicy -Identity $Domain -AuthType Negotiate -MaxPasswordAge 60.00:00:00 -MinPasswordAge 1.00:00:00 -PasswordHistoryCount 24 -ComplexityEnabled $true -ReversibleEncryptionEnabled $true -MinPasswordLength 14
 
 
 
-#all users in the AD should change password at next logon
-#$users= get-aduser -Filter * -SearchBase "CN=Users,DC=$Domain,DC=com"
+#all users in the AD should change password at next logon- only keep this as an option
+<#
 $users= get-aduser -Filter *
 ForEach($user in $users)
     {
-
-       Set-ADUser -PasswordNeverExpires $False -ChangePasswordAtLogon $true -Identity $user -Confirm:$false -WhatIf:$false -ErrorAction Stop
+       Set-ADUser -PasswordNeverExpires $False -ChangePasswordAtLogon $True -Identity $user -Confirm:$false -WhatIf:$false -ErrorAction Stop
                 Write-Verbose -Message 'Change password at logon set to True'
-
     }
+#>
