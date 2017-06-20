e6447a5cf3dd8971afe7393143bfcd845f20f47c
John Molesky <jomolesk@microsoft.com>
2017-06-20 17:01:23 -0400

    Update SSL cert script

--- predeploy/generateCert.ps1
+++ predeploy/generateCert.ps1
@@ -6,6 +6,7 @@ $certPassword = Read-Host -assecurestring "Enter certificate password"
 $cert = New-SelfSignedCertificate -certstorelocation cert:\localmachine\my -dnsname contoso.com
 $path = 'cert:\localMachine\my\' + $cert.thumbprint
 $certPath = $filePath + '\cert.pfx'
+$outFilePath = $filePath + '\cert.txt'
 Export-PfxCertificate -cert $path -FilePath $certPath -Password $certPassword
 $fileContentBytes = get-content $certPath -Encoding Byte
-[System.Convert]::ToBase64String($fileContentBytes) | Out-File 'cert.txt'
+[System.Convert]::ToBase64String($fileContentBytes) | Out-File $outFilePath
